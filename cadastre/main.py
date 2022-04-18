a4f4681804048bfb8e8d0f52604849fbe12f264c
dilip <dilip.bmf@gmail.com>
2022-04-18 19:07:07 +0200

    add origin

--- cadastre/main.py
+++ cadastre/main.py
@@ -10,6 +10,8 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:8181",
+    "https://cadastre-tool.herokuapp.com",
 ]
 
 app.add_middleware(
